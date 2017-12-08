f0434abe384116f1898afad47d809774f8fd2c21
Tom Wolfe <trwolfe13@gmail.com>
2017-12-08 13:13:37 +0000

    Fix type name.

--- src/app/tracker/store/encounters/encounters.actions.ts
+++ src/app/tracker/store/encounters/encounters.actions.ts
@@ -2,10 +2,16 @@ import { Action } from '@ngrx/store';
 import { Encounter } from 'app/tracker/models';
 
 export const ADD_OR_UPDATE_ENCOUNTER = '[Encounters] Add or Update Encounter';
+export const REMOVE_ENCOUNTER = '[Encounters] Add or Update Encounter';
 
 export class AddOrUpdateEncounter implements Action {
   readonly type = ADD_OR_UPDATE_ENCOUNTER;
   constructor(public encounter: Encounter) { }
 }
 
-export type Actions = AddOrUpdateEncounter;
+export class RemoveEncounter implements Action {
+  readonly type = REMOVE_ENCOUNTER;
+  constructor(public encounter: Encounter) { }
+}
+
+export type Action = AddOrUpdateEncounter | RemoveEncounter;

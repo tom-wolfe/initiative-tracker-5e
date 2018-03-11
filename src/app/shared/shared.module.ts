2b9e64237ca0c5a940a1ffbe23a5127e8e5d045d
Tom Wolfe <trwolfe13@gmail.com>
2018-03-11 22:15:03 +0000

    Change input form over to Angular Material.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -17,10 +20,13 @@ const MODULES = [
   CommonModule,
   FormsModule,
   BrowserModule,
+  BrowserAnimationsModule,
   MatToolbarModule,
   MatCardModule,
   MatButtonModule,
-  MatTableModule
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({

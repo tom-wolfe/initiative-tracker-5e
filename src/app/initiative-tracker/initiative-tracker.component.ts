44a2bed73578bef234ac469ad5bea27f30420e95
Tom Wolfe <trwolfe13@gmail.com>
2018-10-29 14:05:52 +0000

    Get hyperlinked encounters working

--- src/app/initiative-tracker/initiative-tracker.component.ts
+++ src/app/initiative-tracker/initiative-tracker.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 import { LoadMonsters } from '../shared/store/monsters';
 import { AppState } from '../store';
 import { CreatureInitiative } from './models/creature-initiative';
-import { AddCreatures, AddCreaturesQS } from './store/encounter';
 
 @Component({
   selector: 'app-initiative-tracker',
@@ -16,18 +14,12 @@ export class InitiativeTrackerComponent {
   initiative: number;
   round: number;
 
-  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+  constructor(private store: Store<AppState>) {
     this.store.dispatch(new LoadMonsters());
 
     const encounter = this.store.select(s => s.tracker.encounter);
     encounter.select(e => e.round).subscribe(r => this.round = r);
     encounter.select(e => e.initiative).subscribe(i => this.initiative = i);
     encounter.select(e => e.creatures).subscribe(c => this.creatures = c);
-
-    this.route.queryParamMap.subscribe(params => {
-      params.getAll('creature').forEach(creature => {
-        this.store.dispatch(new AddCreaturesQS(creature));
-      });
-    });
   }
 }
